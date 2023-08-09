05d29112bb7246ece6004d00085c3cb95edd9245
jonathanmv <jonathanmv@outlook.com>
2023-08-09 14:16:16 +0200

    try to use webcam api

--- src/pages/new.tsx
+++ src/pages/new.tsx
@@ -67,22 +67,19 @@ type WebcamProps = {
 function WebcamComponent({ onCapture }: WebcamProps) {
   const webcamRef = useRef<Webcam>(null);
   const capture = useCallback(() => {
-    let imageSrc = webcamRef.current?.getScreenshot({
-      width: 480,
-      height: 720,
-    });
+    const imageSrc = webcamRef.current?.getScreenshot();
     if (!imageSrc) {
       return alert("No image");
     }
-    const video = document.querySelector("video");
-    if (video) {
-      const canvas = document.createElement("canvas");
-      const context = canvas.getContext("2d");
-      canvas.width = video.videoWidth;
-      canvas.height = video.videoHeight;
-      context?.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
-      imageSrc = canvas.toDataURL("image/jpeg");
-    }
+    // const video = document.querySelector("video");
+    // if (video) {
+    //   const canvas = document.createElement("canvas");
+    //   const context = canvas.getContext("2d");
+    //   canvas.width = video.videoWidth;
+    //   canvas.height = video.videoHeight;
+    //   context?.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
+    //   imageSrc = canvas.toDataURL("image/jpeg");
+    // }
     onCapture(imageSrc);
   }, [onCapture]);
 
