2b3f9a1bbe94fb3e74a87ac650f81c5d6821f667
jonathanmv <jonathanmv@outlook.com>
2023-08-11 17:39:13 +0200

    add animated pato

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { Player } from "@lottiefiles/react-lottie-player";
 import PatoComponent from "~/components/pato";
 import { api } from "~/utils/api";
 
@@ -5,10 +6,21 @@ export default function Home() {
   const { data: patos } = api.patos.getAll.useQuery();
 
   return (
-    <div className="flex flex-col gap-12">
-      {patos?.map((pato) => (
-        <PatoComponent key={pato.id} pato={pato} />
-      ))}
-    </div>
+    <>
+      <div className="flex flex-col gap-12">
+        {patos?.map((pato) => (
+          <PatoComponent key={pato.id} pato={pato} />
+        ))}
+      </div>
+
+      <div className="container mx-auto mt-12 max-w-xs">
+        <Player
+          autoplay
+          loop
+          src="/pato-lottie.json"
+          style={{ height: "300px", width: "300px" }}
+        />
+      </div>
+    </>
   );
 }
